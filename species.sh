b98c705e2b53aeb07aa4c813fb7ab2d61a2e2b6a
Jared Updike <jupdike@thebrain.com>
2015-05-14 14:11:30 -0700

    Run task on all data!

--- species.sh
+++ species.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 mkdir -p ws
-time  bzcat specieswiki-20150426-pages-articles.xml.bz2 | head -n 1000000 | python wikispecies-filter.py
+time  bzcat specieswiki-20150426-pages-articles.xml.bz2 | python wikispecies-filter.py
+#time  bzcat specieswiki-20150426-pages-articles.xml.bz2 | head -n 1000000 | python wikispecies-filter.py
 time  python wikispecies-uniquer.py ws/thoughts.json && mv ws/thoughts.json.uniq ws/thoughts.json
 (cd ws && zip ../ws.brz * && cd ..)
 
